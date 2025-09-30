d119879f4183b18fd4d1107043a0d28a0a526135
m1ccaz <michelle.svanberg@hyperisland.se>
2025-09-30 14:01:00 +0200

    First push with Unity project

---
+++ Assets/Scripts/TapToPlace.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class TapToPlace : MonoBehaviour
+{
+    public GameObject objectToPlace;
+    private ARRaycastManager raycastManager;
+    private static List<ARRaycastHit> hits = new();
+
+    void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 0) return;
+
+        var touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Began) return;
+
+        if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var pose = hits[0].pose;
+            Instantiate(objectToPlace, pose.position, pose.rotation);
+        }
+    }
+}
